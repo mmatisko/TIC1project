36dd9727a60a700fb5d51b02011a9419a7a1c76d
Petr Surý <baltmort@gmail.com>
2017-02-12 13:28:44 +0100

    removed encoding from hash parameter

--- HASHCollision.py
+++ HASHCollision.py
@@ -31,14 +31,14 @@ class HASHCollision(object):
         logging.debug("Length: " + str(self.length))
         logging.debug("Bloom filter size: " + str(self.bloom_filter_size))
 
-        hashed_number = hashlib.sha256(self.number.encode()).hexdigest()[:self.length]
+        hashed_number = hashlib.sha256(self.number).hexdigest()[:self.length]
         self.bf.add(hashed_number)
 
         self.hash_dict[hashed_number] = number
 
         while (True):
             number = hashed_number
-            hashed_number = hashlib.sha256(number.encode()).hexdigest()[:self.length]
+            hashed_number = hashlib.sha256(number).hexdigest()[:self.length]
 
             if (hashed_number in self.bf):
                 self.array_access_counter += 1
